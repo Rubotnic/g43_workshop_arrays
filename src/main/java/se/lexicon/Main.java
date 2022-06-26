3139f715416a6470689823ee708e9b6f96cbf795
Rubotnic <info@rubotnic.com>
2022-06-26 23:57:22 +0200

    Part 2 is done!

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -12,7 +12,7 @@ public class Main {
 //        data[1] = "Simon Elbrink";
 //        data[2] = "Asa Jonsson";
 
-        String data[] = {"Mehradad Javan", "Simon Elbrink", "Asa Jonsson"};
+        String data[] = {"Mehrdad Javan", "Simon Elbrink", "Asa Jonsson"};
         Arrays.sort(data, String.CASE_INSENSITIVE_ORDER);
         NameRepository.setNames(data);
 
@@ -20,10 +20,24 @@ public class Main {
 
         System.out.println("Print Array Data: " + Arrays.toString(NameRepository.findAll()));
 
+        System.out.println("Find By fullname: " +  NameRepository.find("Mehrdad Javan"));
 
+//  add new name
+        boolean result = NameRepository.add("Marcus Gudmonsson");
+        System.out.println(result);
+        System.out.println("Print Array Data: " + Arrays.toString(NameRepository.findAll()));
 
+//  existing name
+        boolean result2 = NameRepository.add("Mehrdad Javan");
+        System.out.println(result2);
+        System.out.println("Print Array Data: " + Arrays.toString(NameRepository.findAll()));
 
-
-
+//  duplicate name
+        boolean result3 = NameRepository.add("Mehrdad Javan");
+        if(result3){
+            System.out.println("Print Array Data: " + Arrays.toString(NameRepository.findAll()));
+        } else {
+            System.out.println("Data os duplicate");
+        }
     }
 }
--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -5,27 +5,64 @@ import java.util.Arrays;
 public class NameRepository {
 
     public static String[] names = new String[0];
-//Returns number of elements in the array
-    public static int getSize(){
+
+    // 1.1 Returns number of elements in the array
+    public static int getSize() {
         return names.length;
     }
 
-//    Sends in an array that set the private static array.
-//    This should replace all existing names.
-    public  static void setNames(String[] names){
+    //    1.2 Sends in an array that set the private static array.
+    //    This should replace all existing names.
+    public static void setNames(String[] names) {
         NameRepository.names = names;
     }
-//    Should completely empty the array.
+
+    //    1.3 Should completely empty the array.
     public static void clear() {
         names = new String[0];
-
     }
-//    Returns all names in a new array
+
+    //    1.4 Returns all names in a new array
     public static String[] findAll() {
         return Arrays.copyOf(names, names.length);
     }
 
+    //    2.1 Returns name if found and null if not found.
+    public static String find(final String fullName) {
 
+        // step 1: cross on array.
+        for (String element: names) {
+
+            // step 2: write a condition to compare element of array with the method param(fullName).
+            if (element.equalsIgnoreCase(fullName)) {
+
+                return element;
+            }
+        }
+        return null;
+    }
+
+    // 2.2 Should add a new name to the array.
+    // Returns true when name was added and false when the array contains the name.
+    public static boolean add(final String fullName) {
+        // step 1: find by full name.
+        // step 2: check if data exit return false, else
+        // step 3: make a copy of array with a new element
+        // step 4: add the method parameter to the last index of the copied array
+        // step 5: replace the copied array with current names array
+        // step 6: return true
+
+        String result = find(fullName);
+
+        if (result != null) {
+            return false;
+        }
+        String[] newArray = Arrays.copyOf(names, names.length + 1);
+        newArray[newArray.length - 1] = fullName;
+        names = newArray;
+
+        return true;
 
     }
+}
 
